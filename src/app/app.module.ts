3b8cdb2b2b4fd4a5df0903e1d8253dc791df6225
Enrique Enciso <enriquejaviere@gmail.com>
2019-03-07 15:05:33 -0600

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WordShufflerDirective } from './directives/word-shuffler.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WordShufflerDirective
   ],
   imports: [
     BrowserModule
---
+++ src/app/directives/word-shuffler.directive.spec.ts
@@ -0,0 +1,8 @@
+import { WordShufflerDirective } from './word-shuffler.directive';
+
+describe('WordShufflerDirective', () => {
+  it('should create an instance', () => {
+    const directive = new WordShufflerDirective();
+    expect(directive).toBeTruthy();
+  });
+});
